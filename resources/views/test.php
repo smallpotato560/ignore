431940806f24ca9858632e2035ea72030bd84d3e
halobangbangboom <tianchen_cc@yeah.net>
2015-08-29 22:22:22 +0800

    v0.1.4

--- resources/views/test.php
+++ resources/views/test.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">God damn it!</div>
+                <?var_dump($result);?>
                 <?foreach($result as $obj):?>
 		            <ul>
 			            <li><?echo $obj['name'];?></li>
