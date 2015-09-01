ebf02402770de49dd26a85f3cd486bbe5052f479
CHEN <tianchen_cc@yeah.net>
2015-09-01 16:11:27 +0800

    update

--- resources/views/list.php
+++ resources/views/list.php
@@ -105,5 +105,9 @@
         </div>
     </div>
 </div>
+<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
