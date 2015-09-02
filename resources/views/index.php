4d175b5fbb6be169fb5034c231c3f884054bdd04
CHEN <tianchen_cc@yeah.net>
2015-09-02 15:27:10 +0800

    update

--- resources/views/index.php
+++ resources/views/index.php
@@ -52,64 +52,269 @@
 <div style="padding: 50px;"></div>
 
 <div class="container">
-    <div class="row">
-        <img class="img-thumbnail img-responsive" src="http://tyimg.h.wap.cn//20131230/5602b899165e6cafadf145986b78a196.jpg">
-        <input type="text">
-        <div class="container-fluid">
-            <div class="row-fluid">
-                <div class="span12">
-                    <div class="carousel slide" id="carousel-758084">
-                        <ol class="carousel-indicators">
-                            <li data-slide-to="0" data-target="#carousel-758084">
-                            </li>
-                            <li data-slide-to="1" data-target="#carousel-758084">
-                            </li>
-                            <li data-slide-to="2" data-target="#carousel-758084" class="active">
-                            </li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="item">
-                                <img alt="" src="http://tyimg.h.wap.cn//20131230/5602b899165e6cafadf145986b78a196.jpg" />
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                            <div class="item">
-                                <img alt="" src="http://tyimg.h.wap.cn//20131230/5602b899165e6cafadf145986b78a196.jpg" />
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                            <div class="item active">
-                                <img alt="" src="http://tyimg.h.wap.cn//20131230/5602b899165e6cafadf145986b78a196.jpg" />
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                        </div> <a data-slide="prev" href="#carousel-758084" class="left carousel-control">‹</a> <a data-slide="next" href="#carousel-758084" class="right carousel-control">›</a>
-                    </div>
-                </div>
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span12">
+                <img class="img-rounded" alt="140x140" src="img/a.jpg" />
             </div>
         </div>
-    </div>
-    <div class="banner">
-        <ul>
-            <li>This is a slide.</li>
-            <li>This is another slide.</li>
-            <li>This is a final slide.</li>
-        </ul>
+        <div class="row-fluid">
+            <div class="span6">
+                <div class="carousel slide" id="carousel-616040">
+                    <ol class="carousel-indicators">
+                        <li class="active" data-slide-to="0" data-target="#carousel-616040">
+                        </li>
+                        <li data-slide-to="1" data-target="#carousel-616040">
+                        </li>
+                        <li data-slide-to="2" data-target="#carousel-616040">
+                        </li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img alt="" src="img/1.jpg" />
+                            <div class="carousel-caption">
+                                <h4>
+                                    棒球
+                                </h4>
+                                <p>
+                                    棒球运动是一种以棒打球为主要特点，集体性、对抗性很强的球类运动项目，在美国、日本尤为盛行。
+                                </p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img alt="" src="img/2.jpg" />
+                            <div class="carousel-caption">
+                                <h4>
+                                    冲浪
+                                </h4>
+                                <p>
+                                    冲浪是以海浪为动力，利用自身的高超技巧和平衡能力，搏击海浪的一项运动。运动员站立在冲浪板上，或利用腹板、跪板、充气的橡皮垫、划艇、皮艇等驾驭海浪的一项水上运动。
+                                </p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img alt="" src="img/3.jpg" />
+                            <div class="carousel-caption">
+                                <h4>
+                                    自行车
+                                </h4>
+                                <p>
+                                    以自行车为工具比赛骑行速度的体育运动。1896年第一届奥林匹克运动会上被列为正式比赛项目。环法赛为最著名的世界自行车锦标赛。
+                                </p>
+                            </div>
+                        </div>
+                    </div> <a data-slide="prev" href="#carousel-616040" class="left carousel-control">‹</a> <a data-slide="next" href="#carousel-616040" class="right carousel-control">›</a>
+                </div>
+            </div>
+            <div class="span6">
+                <ul>
+                    <li>
+                        新闻资讯
+                    </li>
+                    <li>
+                        体育竞技
+                    </li>
+                    <li>
+                        娱乐八卦
+                    </li>
+                    <li>
+                        前沿科技
+                    </li>
+                    <li>
+                        环球财经
+                    </li>
+                    <li>
+                        天气预报
+                    </li>
+                    <li>
+                        房产家居
+                    </li>
+                    <li>
+                        网络游戏
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row-fluid">
+            <div class="span6">
+                <ul>
+                    <li>
+                        新闻资讯
+                    </li>
+                    <li>
+                        体育竞技
+                    </li>
+                    <li>
+                        娱乐八卦
+                    </li>
+                    <li>
+                        前沿科技
+                    </li>
+                    <li>
+                        环球财经
+                    </li>
+                    <li>
+                        天气预报
+                    </li>
+                    <li>
+                        房产家居
+                    </li>
+                    <li>
+                        网络游戏
+                    </li>
+                </ul>
+            </div>
+            <div class="span6">
+                <ul>
+                    <li>
+                        新闻资讯
+                    </li>
+                    <li>
+                        体育竞技
+                    </li>
+                    <li>
+                        娱乐八卦
+                    </li>
+                    <li>
+                        前沿科技
+                    </li>
+                    <li>
+                        环球财经
+                    </li>
+                    <li>
+                        天气预报
+                    </li>
+                    <li>
+                        房产家居
+                    </li>
+                    <li>
+                        网络游戏
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row-fluid">
+            <div class="span6">
+                <ul>
+                    <li>
+                        新闻资讯
+                    </li>
+                    <li>
+                        体育竞技
+                    </li>
+                    <li>
+                        娱乐八卦
+                    </li>
+                    <li>
+                        前沿科技
+                    </li>
+                    <li>
+                        环球财经
+                    </li>
+                    <li>
+                        天气预报
+                    </li>
+                    <li>
+                        房产家居
+                    </li>
+                    <li>
+                        网络游戏
+                    </li>
+                </ul>
+            </div>
+            <div class="span6">
+                <ul>
+                    <li>
+                        新闻资讯
+                    </li>
+                    <li>
+                        体育竞技
+                    </li>
+                    <li>
+                        娱乐八卦
+                    </li>
+                    <li>
+                        前沿科技
+                    </li>
+                    <li>
+                        环球财经
+                    </li>
+                    <li>
+                        天气预报
+                    </li>
+                    <li>
+                        房产家居
+                    </li>
+                    <li>
+                        网络游戏
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row-fluid">
+            <div class="span6">
+                <ul>
+                    <li>
+                        新闻资讯
+                    </li>
+                    <li>
+                        体育竞技
+                    </li>
+                    <li>
+                        娱乐八卦
+                    </li>
+                    <li>
+                        前沿科技
+                    </li>
+                    <li>
+                        环球财经
+                    </li>
+                    <li>
+                        天气预报
+                    </li>
+                    <li>
+                        房产家居
+                    </li>
+                    <li>
+                        网络游戏
+                    </li>
+                </ul>
+            </div>
+            <div class="span6">
+                <ul>
+                    <li>
+                        新闻资讯
+                    </li>
+                    <li>
+                        体育竞技
+                    </li>
+                    <li>
+                        娱乐八卦
+                    </li>
+                    <li>
+                        前沿科技
+                    </li>
+                    <li>
+                        环球财经
+                    </li>
+                    <li>
+                        天气预报
+                    </li>
+                    <li>
+                        房产家居
+                    </li>
+                    <li>
+                        网络游戏
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </div><!-- /.container -->
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script>$(function() {
-        $('.banner').unslider();
-    });</script>
 <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
 <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-<!--unslider-->
-<script src="//code.jquery.com/jquery-latest.min.js"></script>
-<script src="//unslider.com/unslider.js"></script>
 </body>
 </html>
