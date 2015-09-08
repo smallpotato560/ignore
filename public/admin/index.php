69a0141eee2ca3b54b032885001c03298eae60fd
CHEN <tianchen_cc@yeah.net>
2015-09-08 15:10:38 +0800

    test index

--- public/admin/index.php
+++ public/admin/index.php
@@ -48,7 +48,132 @@
     <div class="container-fluid">
         <div class="row-fluid">
             <div class="span12">
-               
+                <img class="img-rounded" alt="140x140" src="img/banner.jpg" />
+            </div>
+        </div>
+    </div>
+    <div class="row-fluid">
+        <div class="span12">
+            <div class="carousel slide" id="carousel-799839">
+                <ol class="carousel-indicators">
+                    <li class="active" data-slide-to="0" data-target="#carousel-799839">
+                    </li>
+                    <li data-slide-to="1" data-target="#carousel-799839">
+                    </li>
+                    <li data-slide-to="2" data-target="#carousel-799839">
+                    </li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img alt="" src="img/banner.jpg" />
+                        <div class="carousel-caption">
+                            <h4>
+                                棒球
+                            </h4>
+                            <p>
+                                棒球运动是一种以棒打球为主要特点，集体性、对抗性很强的球类运动项目，在美国、日本尤为盛行。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img alt="" src="img/banner.jpg" />
+                        <div class="carousel-caption">
+                            <h4>
+                                冲浪
+                            </h4>
+                            <p>
+                                冲浪是以海浪为动力，利用自身的高超技巧和平衡能力，搏击海浪的一项运动。运动员站立在冲浪板上，或利用腹板、跪板、充气的橡皮垫、划艇、皮艇等驾驭海浪的一项水上运动。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img alt="" src="img/banner.jpg" />
+                        <div class="carousel-caption">
+                            <h4>
+                                自行车
+                            </h4>
+                            <p>
+                                以自行车为工具比赛骑行速度的体育运动。1896年第一届奥林匹克运动会上被列为正式比赛项目。环法赛为最著名的世界自行车锦标赛。
+                            </p>
+                        </div>
+                    </div>
+                </div> <a data-slide="prev" href="#carousel-799839" class="left carousel-control">‹</a> <a data-slide="next" href="#carousel-799839" class="right carousel-control">›</a>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span4">
+                <div class="hero-unit">
+                    <h1>
+                        Hello, world!
+                    </h1>
+                    <p>
+                        这是一个可视化布局模板, 你可以点击模板里的文字进行修改, 也可以通过点击弹出的编辑框进行富文本修改. 拖动区块能实现排序.
+                    </p>
+                    <p>
+                        <a class="btn btn-primary btn-large" href="#">参看更多 »</a>
+                    </p>
+                </div>
+            </div>
+            <div class="span4">
+                <ul>
+                    <li>
+                        新闻资讯
+                    </li>
+                    <li>
+                        体育竞技
+                    </li>
+                    <li>
+                        娱乐八卦
+                    </li>
+                    <li>
+                        前沿科技
+                    </li>
+                    <li>
+                        环球财经
+                    </li>
+                    <li>
+                        天气预报
+                    </li>
+                    <li>
+                        房产家居
+                    </li>
+                    <li>
+                        网络游戏
+                    </li>
+                </ul>
+            </div>
+            <div class="span4">
+                <dl>
+                    <dt>
+                        Rolex
+                    </dt>
+                    <dd>
+                        劳力士创始人为汉斯.威尔斯多夫，1908年他在瑞士将劳力士注册为商标。
+                    </dd>
+                    <dt>
+                        Vacheron Constantin
+                    </dt>
+                    <dd>
+                        始创于1775年的江诗丹顿已有250年历史，
+                    </dd>
+                    <dd>
+                        是世界上历史最悠久、延续时间最长的名表之一。
+                    </dd>
+                    <dt>
+                        IWC
+                    </dt>
+                    <dd>
+                        创立于1868年的万国表有“机械表专家”之称。
+                    </dd>
+                    <dt>
+                        Cartier
+                    </dt>
+                    <dd>
+                        卡地亚拥有150多年历史，是法国珠宝金银首饰的制造名家。
+                    </dd>
+                </dl>
             </div>
         </div>
     </div>
