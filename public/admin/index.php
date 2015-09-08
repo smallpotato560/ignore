3785efe76321aa74995cd2e664007927e8000b3e
CHEN <tianchen_cc@yeah.net>
2015-09-08 15:36:35 +0800

    ...

--- public/admin/index.php
+++ public/admin/index.php
@@ -28,6 +28,7 @@
                 <span class="icon-bar"></span>
             </button>
         </div>
+
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">网站首页</a></li>
@@ -45,6 +46,7 @@
 
 <div style="padding: 50px;"></div>
 <div class="container">
+
     <div class="container-fluid">
         <div class="row-fluid">
             <div class="span12">
@@ -52,57 +54,10 @@
             </div>
         </div>
     </div>
-    <div class="row-fluid">
-        <div class="span12">
-            <div class="carousel slide" id="carousel-799839">
-                <ol class="carousel-indicators">
-                    <li class="active" data-slide-to="0" data-target="#carousel-799839">
-                    </li>
-                    <li data-slide-to="1" data-target="#carousel-799839">
-                    </li>
-                    <li data-slide-to="2" data-target="#carousel-799839">
-                    </li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img alt="" src="img/banner.jpg" />
-                        <div class="carousel-caption">
-                            <h4>
-                                棒球
-                            </h4>
-                            <p>
-                                棒球运动是一种以棒打球为主要特点，集体性、对抗性很强的球类运动项目，在美国、日本尤为盛行。
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img alt="" src="img/banner.jpg" />
-                        <div class="carousel-caption">
-                            <h4>
-                                冲浪
-                            </h4>
-                            <p>
-                                冲浪是以海浪为动力，利用自身的高超技巧和平衡能力，搏击海浪的一项运动。运动员站立在冲浪板上，或利用腹板、跪板、充气的橡皮垫、划艇、皮艇等驾驭海浪的一项水上运动。
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img alt="" src="img/banner.jpg" />
-                        <div class="carousel-caption">
-                            <h4>
-                                自行车
-                            </h4>
-                            <p>
-                                以自行车为工具比赛骑行速度的体育运动。1896年第一届奥林匹克运动会上被列为正式比赛项目。环法赛为最著名的世界自行车锦标赛。
-                            </p>
-                        </div>
-                    </div>
-                </div> <a data-slide="prev" href="#carousel-799839" class="left carousel-control">‹</a> <a data-slide="next" href="#carousel-799839" class="right carousel-control">›</a>
-            </div>
-        </div>
-    </div>
+
     <div class="container-fluid">
         <div class="row-fluid">
+
             <div class="span4">
                 <div class="hero-unit">
                     <h1>
@@ -116,6 +71,7 @@
                     </p>
                 </div>
             </div>
+
             <div class="span4">
                 <ul>
                     <li>
@@ -144,6 +100,7 @@
                     </li>
                 </ul>
             </div>
+
             <div class="span4">
                 <dl>
                     <dt>
@@ -175,6 +132,7 @@
                     </dd>
                 </dl>
             </div>
+
         </div>
     </div>
 </div><!-- /.container -->
