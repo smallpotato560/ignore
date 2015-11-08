27d4207cb943818505794f4b2dfa117d65de0460
halobangbangboom <tianchen_cc@yeah.net>
2015-11-08 17:46:15 +0800

    f

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -47,7 +47,7 @@ class LoginController extends Controller
 
         if($user->isExist($all['email'])){
             //已经注册就重定向到首页登入
-            $all=array_splice($all,0,1);
+            array_splice($all,0,1);
             $v = \Auth::validate($all);
             if(!$v)
                 dd("signin failed");
