366d1f7818d6f6135fe2b761bb6eb7e3d212e83a
__codeGentry <tianchen_cc@yeah.net>
2015-11-16 17:43:49 +0800

    merge

--- resources/views/admin/content.blade.php
+++ resources/views/admin/content.blade.php
@@ -2,9 +2,9 @@
 <div class="span10 column ui-sortable" id="show" style="overflow: auto">
     <div class="container-fluid">
         <div class="row-fluid">
-            <h1>欢迎使用后台管理系统</h1>
+            <h1>鍚庡彴绠＄悊绯荤粺</h1>
             <div class="input-append date" id="datetimepicker" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
-                <label for="datetimepicker">现在时间</label>
+                <label for="datetimepicker">鐜板湪鏃堕棿</label>
                 <input size="16" type="text" value="12-02-2012" readonly>
                 <span class="add-on"><i class="icon-th"></i></span>
                 <canvas class="CoolClock"></canvas>
