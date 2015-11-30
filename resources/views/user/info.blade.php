a455f30e5014ceca4bcad72f98a2c205e9274c33
__codeGentry <tianchen_cc@yeah.net>
2015-11-30 09:55:01 +0800

    merge

--- resources/views/user/info.blade.php
+++ resources/views/user/info.blade.php
@@ -1,6 +1,6 @@
 @section("info")
     <div class="row">
-        <div class="panel panel-default col-lg-3 col-md-3" style="padding: 0 !important;">
+        <div class="col-lg-3 col-md-3 panel panel-default"  style="padding: 0 !important;">
             <div class="panel-heading" >
 
                 <div class="panel-title">
@@ -43,7 +43,6 @@
 
             </div>
         </div>
-
         <div class="col-lg-9 col-md-9">
             <div class="container-fluid">
                 {{--<div class="row">--}}
